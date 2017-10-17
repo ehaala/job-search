b3a241451df3d46e74db3c7f743b92676158d7c8
Elizabeth friedland <elizabethfriedland@Elizabeths-MacBook.local>
2017-10-17 11:18:59 -0700

    first commit

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -3,29 +3,12 @@ import './index.css';
 
 
 class Homepage extends Component {
-  // constructor(props){
-	// 	super(props)
-	// 	this.state = {
-	// 	  jobs: []
-	
-	// 	}
-	// 	this.componentDidMount = this.componentDidMount.bind(this)
-	//   }
-	//   componentDidMount(){
-	// 	fetch('/jobs')
-	// 	  .then(response => response.json())
-	// 	  .then(response => this.setState({jobs: response}))
-		  
-	//   }
+
   render() {
     return (
       <div>
       <h1 className="App"> This is the Homepage</h1>
-      {/* <ul className="App">
-       
-        {this.state.jobs.map((item, index) => (<li key={index}>{item.title} </li>))}
-        
-      </ul> */}
+      
 
       
 
