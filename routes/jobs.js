9e1ac42cc9293af7a9a958aabd4aabd977aa57e0
stevens1434 <stevens1434@gmail.com>
2017-10-17 11:22:13 -0700

    double checked that the app.js routes and models were set up so - they look good ----- tested database connection by adding something to the database - it worked ---- tested backend by finding something form the database and res.sending it to the backend Port (3001) - it worked ---- TO DO: create CRUD route in routes to ADD item to db based on user selection from search results, FIND item to display in favorites page, DELETE item from favorites page. Also need to create a favorites page on client side

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,22 +8,26 @@ var router = express.Router();
 
 /* GET animals listing. */
 router.get('/', function(req, res, next) {
-    // Job.create({
-        
-    //         title: 'Mid Dev',
-    //         companyName: 'Google',
-    //         location: 'Kirkland'
-    //     }, function(err,job){
-    //         if(err) return console.log(err);
-    //         res.send(job);
-        
-    // })
-  //res.send(dev);
-  //console.log('in the /jobs roite');
-  Job.find({}, function(err, jobs) {
+
+  Job.create({ //added to test databasae, not needed for functional app
+    location: 'Seattle',
+    company: 'General Assembly',
+    title: 'Web Developer',
+    meta: {
+      contactPerson: 'Boris Grishenko',
+      contactEmail: 'Boris@bondvillainsurvivors.com'
+    }
+    }, function(err, job) {
+      if(err) return console.log(err)
+      res.send(job)
+  })
+
+  Job.find({}, function(err, jobs) {//must be w/in a GET/POST/etc route
       if(err)return console.log(err);
       res.send(jobs);
   });
  });
+
+
 
 module.exports = router;
