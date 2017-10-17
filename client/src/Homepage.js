3dbf217e043da7bdacef7ae2e36c9fce48511908
Elizabeth friedland <elizabethfriedland@Elizabeths-MacBook.local>
2017-10-17 16:07:28 -0700

    something

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -75,29 +75,29 @@ class Homepage extends Component {
 
         <div className=" belowImg">
             <div className="card radius shadowDepth1">
-                <div className="card__content card__padding">
+              <div className="card__content card__padding">
                 <div className="card__meta">
                   <a >Location: {this.state.location}</a>
                     <time>Post Date: {this.state.postDate}</time>
                 </div>
 
-					<article className="card__article">
-						<h2><a>Title: {this.state.title}</a></h2>
-              <p>Search key: {this.state.value}</p>
-              <p>Apply <a href={this.state.url} target="_blank">here!</a></p>
-					</article>
+                <article className="card__article">
+                  <h2><a>Title: {this.state.title}</a></h2>
+                    
+                    <p>Apply <a href={this.state.url} target="_blank">here!</a></p>
+                </article>
+                      </div>
+
+                <div className="card__action">
+
+                  <div className="card__author">
+                    <img src="http://lorempixel.com/40/40/sports/" alt="user" />
+                    <div className="card__author-content">
+                    <h3>Company: {this.state.company}</h3>
+                    <a>add to favorites</a>
+                    </div>
+                  </div>
                 </div>
-
-					<div className="card__action">
-
-						<div className="card__author">
-							<img src="http://lorempixel.com/40/40/sports/" alt="user" />
-							<div className="card__author-content">
-              <h3>Company: {this.state.company}</h3>
-               <a>add to favorites</a>
-							</div>
-						</div>
-					</div>
              </div>	            
         </div>
       </div>
