9d9e28e961e7339fbc6ecf88172172f2ea77819e
Elizabeth friedland <elizabethfriedland@Elizabeths-MacBook.local>
2017-10-17 17:46:53 -0700

    Almost nvp

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './index.css';
+import Items from './Items';
 
 
 
@@ -7,12 +8,8 @@ class Homepage extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      title: "",
-      company: "",
-      location: "",
-      url: "",
-      postDate: "",
-      value: "seattle"
+      items: [],
+      value: 'california'
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -27,11 +24,7 @@ class Homepage extends Component {
     fetch(full)
       .then(response => response.json())
       .then(response => this.setState({
-        title: response.listings.listing[0].title,
-        company: response.listings.listing[0].company.name,
-        location: response.listings.listing[0].company.location.name,
-        url: response.listings.listing[0].apply_url,
-        postDate: response.listings.listing[0].post_date
+        items: response.listings.listing
       }))
   }
 
@@ -53,55 +46,19 @@ class Homepage extends Component {
     fetch(full)
       .then(response => response.json())
       .then(response => response.listings.listing.length && this.setState({
-        title: response.listings.listing[0].title,
-        company: response.listings.listing[0].company.name,
-        location: response.listings.listing[0].company.location.name,
-        url: response.listings.listing[0].apply_url,
-        postDate: response.listings.listing[0].post_date
+        items: response.listings.listing
       }))
   }
 
   render() {
+    let listings = this.state.items.map((item, index) => (
+      <Items item={item} key={index} />
+      ))
+
     return (
       <div className='App'>
-          <div className="card__image border-tlr-radius firstImg">
-          <img src="https://static.pexels.com/photos/27406/pexels-photo-27406.jpg" alt="animal" className="border-tlr-radius" />
-            <form className="formOverImg" onSubmit={this.handleSubmit}>
-              <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Keywords"/>
-              <button type="submit">Search!</button>
-          </form>
-        </div>
-
-        <div className=" belowImg">
-            <div className="card radius shadowDepth1">
-              <div className="card__content card__padding">
-                <div className="card__meta">
-                  <a >Location: {this.state.location}</a>
-                    <time>Post Date: {this.state.postDate}</time>
-                </div>
-
-           
-          
-					<article className="card__article">
-						<h2><a>Title: {this.state.title}</a></h2>
-              <p>Search key: {this.state.value}</p>
-              <p>Apply <a href={this.state.url} target="_blank">here!</a></p>
-					</article>
-                </div>
-
-
-                <div className="card__action">
-
-                  <div className="card__author">
-                    <img src="http://lorempixel.com/40/40/sports/" alt="user" />
-                    <div className="card__author-content">
-                    <h3>Company: {this.state.company}</h3>
-                    <a>add to favorites</a>
-                    </div>
-                  </div>
-                </div>
-             </div>	            
-        </div>
+          <h1>Job Search</h1>
+          {listings}
       </div>
     );
   }
---
+++ client/src/Items.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+import './index.css';
+
+class Items extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: props.item.title,
+      company: props.item.company.name,
+      location: props.item.company.location.name,
+      url: props.item.apply_url,
+      postDate: props.item.post_date
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+
+        <div className=" belowImg">
+            <div className="card radius shadowDepth1">
+                <div className="card__content card__padding">
+                <div className="card__meta">
+                  <a >Location: {this.state.location}</a>
+                    <time>Post Date: {this.state.postDate}</time>
+                </div>
+           
+          <article className="card__article">
+            <h2><a>Title: {this.state.title}</a></h2>
+              <p>Apply <a href={this.state.url} target="_blank">here!</a></p>
+          </article>
+                </div>
+
+          <div className="card__action">
+
+            <div className="card__author">
+              <img src="http://lorempixel.com/40/40/sports/" alt="user" />
+              <div className="card__author-content">
+              <h3>Company: {this.state.company}</h3>
+               <a href="#">add to favorites</a>
+              </div>
+            </div>
+          </div>
+             </div>             
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Items;
